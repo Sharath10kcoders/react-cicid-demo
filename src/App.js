7294a11e168036ea0849df4faf2dfeafb2d97d0a
sharathErt <sharathepalapally@errortech.in>
2024-04-22 11:08:35 +0530

    added few lines

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello world this test branch</h1>
+        <h1>merging test brach code with prod</h1>
       </header>
     </div>
   );
